6ed711e9d700b0444db2d42c96073c4689a33ef9
brendan <rooneybb2@gmail.com>
2021-02-21 02:46:35 -0600

    basic foundational template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+// running from index.js because otherwise background color is not honored
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/header';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Registry from './components/Registry';
+import RSVP from './components/RSVP';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <React.StrictMode>
+                <Router>
+                    <Header />
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/About" component={About} />
+                        <Route path="/Contact" component={Contact} />
+                        <Route path="/Registry" component={Registry} />
+                        <Route path="/RSVP" component={RSVP} />
+                    </Switch>
+                </Router>
+            </React.StrictMode>
+        );
+    }
 }
 
 export default App;
